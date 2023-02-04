2e5b2d26f7deca289b4c941ab6aa7ddbd9689a8e
tmunkhb <tmunkhb@gmail.com>
2023-02-03 21:04:59 -0500

    home page layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from "next/head";
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin,} from 'react-icons/ai';
+import Image from "next/image";
+import deved from "../public/peeps-avatar.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 export default function Home() {
   return (
@@ -12,15 +18,29 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedby</h1>
+            <h1 className="text-xl font-burtons">developedby</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
               </li>
-              <li><a href="#">Resume</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">About</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Projects</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Contact</a></li>
             </ul>
           </nav>
-
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Temuulen Munkhbat</h2>
+            <h3 className="text=2xl py-2">Software Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">Cofee, Code, Kids!</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillGithub />
+            <AiFillLinkedin />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover"/>
+          </div>
         </section>
       </main>
     </div>
